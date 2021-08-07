ee9989c00b90278b767ca373855f774e721d2321
Pehachain <pehachain@gmail.com>
2021-08-08 01:22:40 +0530

    Error

--- app/src/main/java/com/example/travelg/ScrollingActivity.java
+++ app/src/main/java/com/example/travelg/ScrollingActivity.java
@@ -2,18 +2,11 @@ package com.example.travelg;
 
 import android.content.Intent;
 import android.os.Bundle;
-
-import com.google.android.material.appbar.CollapsingToolbarLayout;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class ScrollingActivity extends AppCompatActivity {
 
@@ -69,4 +62,6 @@ public class ScrollingActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+
 }
