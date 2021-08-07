24224a6b8e3b3c3b255b115d327c936f09222d85
RoyGeorge29 <roygeorgekala@gmail.com>
2021-08-07 22:15:31 +0530

    Solving crashing bug

--- app/src/main/java/com/example/travelg/ScrollingActivity.java
+++ app/src/main/java/com/example/travelg/ScrollingActivity.java
@@ -22,12 +22,14 @@ public class ScrollingActivity extends AppCompatActivity {
         try {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_scrolling);
+/*
             Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
             setSupportActionBar(toolbar);
             CollapsingToolbarLayout toolBarLayout = (CollapsingToolbarLayout) findViewById(R.id.toolbar);
             toolBarLayout.setTitle(getTitle());
-
-            /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+*/
+/*
+            FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
             fab.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
