315ea384bccfd189b063cf4bd55956286cfe541a
RoyGeorge29 <roygeorgekala@gmail.com>
2021-08-08 09:55:29 +0530

    Fixing prior errors and adding connectional features

--- app/src/main/java/com/example/travelg/ScrollingActivity.java
+++ app/src/main/java/com/example/travelg/ScrollingActivity.java
@@ -1,15 +1,30 @@
 package com.example.travelg;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import static java.nio.charset.StandardCharsets.UTF_8;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 
 public class ScrollingActivity extends AppCompatActivity {
 
+    TextView cityname,descriptionView;
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         try {
@@ -34,11 +49,34 @@ public class ScrollingActivity extends AppCompatActivity {
         }catch (Exception e){
             e.printStackTrace();
             Toast.makeText(ScrollingActivity.this,e.toString(),Toast.LENGTH_LONG).show();
-
         }
         Intent intent = getIntent();
         String city = intent.getStringExtra("City");
-        Toast.makeText(ScrollingActivity.this,"Current city is " + city,Toast.LENGTH_SHORT).show();
+
+
+        cityname = findViewById(R.id.cityNameView);
+        descriptionView = findViewById(R.id.descriptionView);
+
+        try {
+            InputStream is = getAssets().open("cities.json");
+            int size = is.available()+1;
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            String json = new String(buffer, UTF_8);
+            JSONArray ja = new JSONArray(json);
+            for (int i = 0; i<ja.length();i++){
+                JSONObject jo = ja.getJSONObject(i);
+                if (jo.getString("city").equals(city)){
+                    /*Toast.makeText(ScrollingActivity.this,"Current city is " + city+" and some data is " + jo.getString("description"),Toast.LENGTH_SHORT).show();*/
+                    cityname.setText(jo.getString("city"));
+                    descriptionView.setText(jo.getString("description"));
+                }
+            }
+
+        } catch (IOException | JSONException e) {
+            e.printStackTrace();
+        }
+
 
     }
 
